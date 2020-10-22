5a784e60624e8ec5c1be0ffdb30976d1550562ed
takaking216 <takaking216@yahoo.co.jp>
2020-10-22 17:39:14 +0900

    firebase接続 streambuilderによる出力 cloudstoreに投稿確認まで。

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('chats/XWeVmF3n0MfWnPEiCW7L/messages')
+            .snapshots(),
+        builder: (ctx, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final documents = streamSnapshot.data.documents;
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (ctx, index) => Container(
+              padding: EdgeInsets.all(8),
+              child: Text(documents[index]['text']),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/XWeVmF3n0MfWnPEiCW7L/messages')
+              .add({'text': 'this was added by click button'});
+        },
+      ),
+    );
+  }
+}
