0bb45eaefdd1cf88cab196f8398435da74be9d37
takaking216 <takaking216@yahoo.co.jp>
2020-10-27 18:10:41 +0900

    ログアウト実装完了、typoっぽかった 入力フォーム作成 投稿テキスト装飾

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,5 @@
+import 'package:chat/chat/messages.dart';
+import 'package:chat/chat/new_message.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -39,33 +41,15 @@ class ChatScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: StreamBuilder(
-        stream: Firestore.instance
-            .collection('chats/XWeVmF3n0MfWnPEiCW7L/messages')
-            .snapshots(),
-        builder: (ctx, streamSnapshot) {
-          if (streamSnapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          final documents = streamSnapshot.data.documents;
-          return ListView.builder(
-            itemCount: documents.length,
-            itemBuilder: (ctx, index) => Container(
-              padding: EdgeInsets.all(8),
-              child: Text(documents[index]['text']),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: Messages(),
             ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          Firestore.instance
-              .collection('chats/XWeVmF3n0MfWnPEiCW7L/messages')
-              .add({'text': 'this was added by click button'});
-        },
+            NewMessage(),
+          ],
+        ),
       ),
     );
   }
